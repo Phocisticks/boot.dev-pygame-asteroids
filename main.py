85343c3641db01c4de2dfc7afaaf69e988fce3c0
Mark Glose <bxbitbx@gmail.com>
2025-06-02 03:32:36 -0500

    using groups

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from constants import *
 from player import Player
 
 def main():    
-    pygame.init()
+    pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
@@ -15,10 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
                 
         screen.fill("black")
-        player.draw(screen)
+        
+        for d in drawable:
+            d.draw(screen)
+            
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
